b1a3ad6e09acd867e158c66ee45f75e7de8f35ec
Adrian11003 <adrian11003@gmail.com>
2024-05-08 04:09:15 -0500

    login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,15 @@
+import styles from "@/app/ui/login/login.module.css";
 const LoginPage = () => {
-    return (
-        <div>LoginPage</div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="Usuario" />
+        <input type="password" placeholder="Contraseña" />
+        <button>Iniciar Sesión</button>
+      </form>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
